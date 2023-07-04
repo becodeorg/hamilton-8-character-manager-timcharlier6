a7c2f46946c49bb424196e46736bd06ff9672305
cedricwietkin <wietkin.c@gmail.com>
2023-07-04 12:13:18 +0200

    presque fini, resque plus que cette image que je n arrive pas a uploade

--- scripts/create-character-script.js
+++ scripts/create-character-script.js
@@ -58,3 +58,58 @@ form.addEventListener("submit", function(event) {
   event.preventDefault();
   displayCardWithData();
 });
+// Set the HTML content of cardInfoCharacter
+cardInfoCharacter.innerHTML = 
+`
+<div class="imgContainer">
+    <img src="data:image/png;base64, ${cardData['image']}"> 
+</div>
+<h1 class="name"> ${cardData["name"]} </h1>
+<p class="description"> </p>
+<button class="editButton">Edit</button>
+<button class="deleteButton">Delete</button>
+`;
+
+document.querySelector('.description').innerHTML =
+    marked.parse(cardData.description);
+
+const editButton = cardInfoCharacter.querySelector(".editButton");
+// Add click event listener to editButton
+editButton.addEventListener("click", () => {
+    // Redirect the user to the details page by passing the ID in the URL parameters
+    window.location.href = `character-editor.html?id=${cardData["id"]}`;
+});
+
+// Add click event listener to deleteButton for deleting the character
+const deleteButton = cardInfoCharacter.querySelector('.deleteButton');
+deleteButton.addEventListener('click', () => {
+    // Display an alert to confirm the deletion
+    const confirmation = confirm('Are you sure you want to delete?');
+    if (confirmation) {
+        // Add code to delete the character
+        fetch(`https://character-database.becode.xyz/characters/${cardData["id"]}`, {
+            method: 'DELETE',
+        })
+        .then(response => {
+            if (response.ok) {
+                // Display success message
+                alert("You killed him :'(");
+
+                // Redirect to index.html
+                window.location.href = "index.html";
+            } else {
+                // Handle the error case
+                alert("Failed to delete character.");
+            }
+        })
+        .catch(error => {
+            // Handle any network or other errors
+            console.error("Error deleting character:', error");
+            alert('An error occurred while deleting the character.');
+        });
+    }
+});
+
+// Append cardInfoCharacter to cardContainer
+cardContainer.appendChild(cardInfoCharacter);
+return cardInfoCharacter;
