d36e0446cff15e053c73a1bdc5bcb175d5f8a93e
CMarchandon <camillemarchandon3@hotmail.fr>
2023-07-04 13:37:05 +0200

    updates

--- scripts/character-edit-script.js
+++ scripts/character-edit-script.js
@@ -1,81 +1,99 @@
-const cardEditorCharacter = document.querySelector('.cardEditorCharacter');
-let formulaire = document.querySelector('form');
+// Function to convert image to base64
+document.getElementById("image").addEventListener("change", readFile);
+let image64 = null;
 
-class Character {
-    constructor (id) {
-      this.id = id;
-      this.name = "";
-      this.description = "";
-      this.image = "";
-    }
-  
-    getName () {
-      return this.name;
-    }
-  
-    setName (characterName){
-      this.name = characterName;
-    }
-  
-    getDescription () {
-      return this.description;
-    }
-  
-    setDescription (characterDescription) {
-      this.description = characterDescription;
-    }
-  
-    getImage () {
-      return this.image;
-    }
-  
-    setImage (characterImage) {
-      this.image = characterImage;
-    }
-  }
-  
-  // Get the 'id' parameter from the URL
-const urlId = (new URLSearchParams(window.location.search)).get('id');
-  
-  // Fetch the character data based on the URL ID
-fetch('https://character-database.becode.xyz/characters/' + urlId)
-    .then(response => response.json())
-    .then(cardData => { 
-      const myCharacter = new Character(cardData.id);
-   
-      const inputImage = formulaire.querySelector('.inputImage'); 
-      const inputName = formulaire.querySelector('.inputName'); 
-      const inputDescription = formulaire.querySelector('.inputDescription'); 
+function readFile() {
+  if (!this.files || !this.files[0]) return;
+  const FR = new FileReader();
+  FR.addEventListener("load", function(evt) {
+    image64 = evt.target.result.substring(evt.target.result.indexOf(',') + 1);
+  });
+  FR.readAsDataURL(this.files[0]);
+}
 
-      inputName.setAttribute("placeholder", cardData.name);
-      inputDescription.setAttribute("placeholder", cardData.description);
-  
-      const imageValue = inputImage.value; 
-      const nameValue = inputName.value; 
-      const descriptionValue = inputDescription.value; 
-  
-      myCharacter.setName(nameValue);
-      myCharacter.setDescription(descriptionValue);
-      myCharacter.setImage(imageValue);
-  
-      const requestBody = {
-        name: myCharacter.getName(),
-        description: myCharacter.getDescription(),
-        image: myCharacter.getImage()
-      };
+// Function to fetch original character data from the API
+function fetchOriginalCharacterData(characterId) {
+  return fetch(`https://character-database.becode.xyz/characters/${characterId}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch original character data.");
+      }
     });
+}
 
-fetch('https:/character-database.becode.xyz/characters', {
-    method : 'post',
+// Function to populate the form with default values
+function populateFormWithCharacterData(characterData) {
+  document.querySelector("#name").value = characterData.name;
+  document.querySelector("#shortDesc").value = characterData.shortDescription;
+  document.querySelector("#description").value = characterData.description;
+  document.querySelector("#image").setAttribute("data-original-image", characterData.image);
+  document.querySelector("#image-preview").src = characterData.image;
+}
+
+// Function to fetch card data and display it
+function displayCardWithData() {
+  const inputName = document.querySelector("#name").value;
+  const inputShortDesc = document.querySelector("#shortDesc").value;
+  const inputDescription = document.querySelector("#description").value;
+
+  const originalImage = document.querySelector("#image").getAttribute("data-original-image");
+
+  const imageValue = image64 !== null ? image64 : originalImage;
+
+  const init = {
+    method: 'PUT',
     headers: {
-        'content-type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body : JSON.stringify(requestBody)
-    })
-    .then(res => {
-    alert("Request complete! response:" + res);
+    body: JSON.stringify({
+      name: inputName,
+      shortDescription: inputShortDesc,
+      description: inputDescription,
+      image: imageValue,
+    }),
+  };
+
+  // Fetch data from the API endpoint
+  fetch(`https://character-database.becode.xyz/characters/${characterId}`, init)
+    .then(response => {
+      if (response.ok) {
+        // Display success message
+        alert("Character modified");
+        // Redirect to index.html
+        window.location.href = "index.html";
+      } else {
+        // Handle the error case
+        throw new Error("Failed to modify character.");
+      }
     })
     .catch(error => {
-    console.log(error);
+      // Log and display the error message
+      console.log(error);
+      alert(error.message);
     });
+}
 
+// Get the character ID from the URL or any other source
+const urlParams = new URLSearchParams(window.location.search);
+const characterId = urlParams.get('id');
+
+// Call the fetchOriginalCharacterData function to get the original character details
+fetchOriginalCharacterData(characterId)
+  .then(characterData => {
+    // Populate the form with the default values from the original character
+    populateFormWithCharacterData(characterData);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// Call the displayCardWithData function when the form is submitted
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  displayCardWithData();
+});
+
+//This part was made with the Help of chat.GPT//
--- scripts/character-info-script.js
+++ scripts/character-info-script.js
@@ -1,69 +1,71 @@
 function createCard(cardData) {
-  const cardInfoCharacter = document.querySelector(".cardInfoCharacter");
+    const cardInfoCharacter = document.querySelector(".cardInfoCharacter");
+  
+    // Set the HTML content of cardInfoCharacter
+    cardInfoCharacter.innerHTML = `
+      <div class="imgContainer">
+          <img src="data:image/png;base64, ${cardData['image']}"> 
+      </div>
+      <h1 class="name">${cardData["name"]}</h1>
+      <p id="#description">${cardData["description"]}</p>
+      <button class="editButton">Edit</button>
+      <button class="deleteButton">Delete</button>
+    `;
 
-  // Set the HTML content of cardInfoCharacter
-  cardInfoCharacter.innerHTML = 
-  `
-  <div class="imgContainer">
-      <img src="data:image/png;base64, ${cardData['image']}"> 
-  </div>
-  <h1 class="name"> ${cardData["name"]} </h1>
-  <p class="description"> ${cardData["description"]}</p>
-  <button class="editButton">Edit</button>
-  <button class="deleteButton">Delete</button>
-  `;
+    //Convert mardown from description to HTML
+    document.getElementById('#description').innerHTML = marked.parse(cardData["description"]);
 
-  const editButton = cardInfoCharacter.querySelector(".editButton");
-  // Add click event listener to editButton
-  editButton.addEventListener("click", () => {
+    const editButton = cardInfoCharacter.querySelector(".editButton");
+    // Add click event listener to editButton
+    editButton.addEventListener("click", () => {
       // Redirect the user to the details page by passing the ID in the URL parameters
       window.location.href = `character-editor.html?id=${cardData["id"]}`;
-  });
-
-  // Add click event listener to deleteButton for deleting the character
-  const deleteButton = cardInfoCharacter.querySelector('.deleteButton');
-  deleteButton.addEventListener('click', () => {
+    });
+  
+    // Add click event listener to deleteButton for deleting the character
+    const deleteButton = cardInfoCharacter.querySelector('.deleteButton');
+    deleteButton.addEventListener('click', () => {
       // Display an alert to confirm the deletion
       const confirmation = confirm('Are you sure you want to delete?');
       if (confirmation) {
-          // Add code to delete the character
-          fetch(`https://character-database.becode.xyz/characters/${cardData["id"]}`, {
-              method: 'DELETE',
+        // Add code to delete the character
+        fetch(`https://character-database.becode.xyz/characters/${cardData["id"]}`, {
+            method: 'DELETE',
           })
           .then(response => {
-              if (response.ok) {
-                  // Display success message
-                  alert("You killed him :'(");
-
-                  // Redirect to index.html
-                  window.location.href = "index.html";
-              } else {
-                  // Handle the error case
-                  alert("Failed to delete character.");
-              }
+            if (response.ok) {
+              // Display success message
+              alert("You killed him :'(");
+  
+              // Redirect to index.html
+              window.location.href = "index.html";
+            } else {
+              // Handle the error case
+              alert("Failed to delete character.");
+            }
           })
           .catch(error => {
-              // Handle any network or other errors
-              console.error("Error deleting character:', error");
-              alert('An error occurred while deleting the character.');
+            // Handle any network or other errors
+            console.error("Error deleting character:", error);
+            alert('An error occurred while deleting the character.');
           });
       }
-  });
-
-  // Append cardInfoCharacter to cardContainer
-  cardContainer.appendChild(cardInfoCharacter);
-  return cardInfoCharacter;
-}
-
-// Get the 'id' parameter from the URL
-const urlId = (new URLSearchParams(window.location.search)).get('id');
-
-// Fetch the character data based on the URL ID
-fetch('https://character-database.becode.xyz/characters/' + urlId)
-.then(response => response.json())
-.then(cardData => createCard(cardData))
-.catch(error => {
-  console.log(error);
-});
-
-
+    });
+  
+    // Append cardInfoCharacter to cardContainer
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.appendChild(cardInfoCharacter);
+    return cardInfoCharacter;
+  }
+  
+  // Get the 'id' parameter from the URL
+  const urlId = new URLSearchParams(window.location.search).get('id');
+  
+  // Fetch the character data based on the URL ID
+  fetch(`https://character-database.becode.xyz/characters/${urlId}`)
+    .then(response => response.json())
+    .then(cardData => createCard(cardData))
+    .catch(error => {
+      console.log(error);
+    });
+  
